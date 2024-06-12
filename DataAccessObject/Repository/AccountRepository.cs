0b699f42f0c9f67d2ed7f5d387241824d050ef88
Nguyen Duc Anh <anhndse173319@fpt.edu.vn>
2024-06-12 08:06:57 +0700

    feat(account): adding CRU account feat

---
+++ DataAccessObject/Repository/AccountRepository.cs
@@ -0,0 +1,47 @@
+﻿using BussinessObject.Model.Entities;
+using DataAccessObject.Database;
+using DataAccessObject.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessObject.Repository
+{
+    public class AccountRepository : BaseRepository<User>, IAccountRepository
+    {
+        private readonly PetManagementContext _context;
+
+        public AccountRepository(PetManagementContext context):base(context) 
+        {
+            _context = context;
+        }
+
+        public async Task<User> CreateAccountRepo(User user)
+        {
+            _context.Add(user); 
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public Task<User> GetAccountDetailByEmail(string email)
+        {
+            var data = _context.Users.SingleOrDefaultAsync(u => u.Email == email);
+            return data;
+        }
+
+        public Task<User> GetAccountDetailById(int id)
+        {
+            var data = _context.Users.SingleOrDefaultAsync(u => u.Id == id);
+            return data;
+        }
+
+        public async Task<List<User>> GetAllAccount()
+        {
+            var data = await _context.Users.ToListAsync();  
+            return data;
+        }
+    }
+}
